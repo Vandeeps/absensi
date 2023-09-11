1df75eb59d1cc8426b8da93b5a65d454b400984d
Vandeeps <142085832+Vandeeps@users.noreply.github.com>
2023-09-11 12:15:02 +0800

    Add files via upload

---
+++ webabsen (2).sql
@@ -0,0 +1,165 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 11, 2023 at 06:05 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `webabsen`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `data_karyawan`
+--
+
+CREATE TABLE `data_karyawan` (
+  `NIP` varchar(16) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `alamat` varchar(100) NOT NULL,
+  `hp` varchar(16) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `data_karyawan`
+--
+
+INSERT INTO `data_karyawan` (`NIP`, `nama`, `alamat`, `hp`) VALUES
+('1', 'Zefanya', 'Sultan Adam', '09787819293192'),
+('2', 'Tomi', 'Sultan Adam', '091089102931');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lap_keg`
+--
+
+CREATE TABLE `lap_keg` (
+  `Id_lap` int(11) NOT NULL,
+  `NIP` varchar(100) NOT NULL,
+  `laporan` varchar(100) NOT NULL,
+  `ket` enum('Selesai','Proses','Belum') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `lap_keg`
+--
+
+INSERT INTO `lap_keg` (`Id_lap`, `NIP`, `laporan`, `ket`) VALUES
+(1, '1', 'Sakit', 'Selesai');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+
+CREATE TABLE `login` (
+  `id_login` int(11) NOT NULL,
+  `user` varchar(255) NOT NULL,
+  `pass` char(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `login`
+--
+
+INSERT INTO `login` (`id_login`, `user`, `pass`) VALUES
+(1, 'Zeff', 'mamah'),
+(2, 'user', 'user');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_absen`
+--
+
+CREATE TABLE `tb_absen` (
+  `No` varchar(10) NOT NULL,
+  `NIP` varchar(16) NOT NULL,
+  `absen` enum('Hadir','Sakit','Izin') NOT NULL,
+  `Ket` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_absen`
+--
+
+INSERT INTO `tb_absen` (`No`, `NIP`, `absen`, `Ket`) VALUES
+('1', '1', 'Sakit', 'Demam'),
+('2', '2', 'Hadir', 'Hadir');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `data_karyawan`
+--
+ALTER TABLE `data_karyawan`
+  ADD PRIMARY KEY (`NIP`);
+
+--
+-- Indexes for table `lap_keg`
+--
+ALTER TABLE `lap_keg`
+  ADD PRIMARY KEY (`Id_lap`),
+  ADD KEY `NIP` (`NIP`);
+
+--
+-- Indexes for table `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`id_login`);
+
+--
+-- Indexes for table `tb_absen`
+--
+ALTER TABLE `tb_absen`
+  ADD PRIMARY KEY (`No`),
+  ADD KEY `NIP` (`NIP`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `login`
+--
+ALTER TABLE `login`
+  MODIFY `id_login` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `lap_keg`
+--
+ALTER TABLE `lap_keg`
+  ADD CONSTRAINT `lap_keg_ibfk_1` FOREIGN KEY (`NIP`) REFERENCES `data_karyawan` (`NIP`);
+
+--
+-- Constraints for table `tb_absen`
+--
+ALTER TABLE `tb_absen`
+  ADD CONSTRAINT `tb_absen_ibfk_1` FOREIGN KEY (`NIP`) REFERENCES `data_karyawan` (`NIP`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
