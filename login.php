a1ccde290ce5d8c5d247e1bbe72ba3281216c6dc
Vandeeps <142085832+Vandeeps@users.noreply.github.com>
2023-09-12 15:11:44 +0800

    membenahi login

--- login.php
+++ login.php
@@ -8,13 +8,28 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body>
+    <style>
+        .card{
+            background-color:#61677A;
+        }
+        label{
+            color:white;
+        }
+        body {
+      background-image: url('mantap.png'); /* Ganti dengan path gambar Anda */
+      background-size:cover;
+      background-position: center;
+      background-repeat: repeat;
+    }
+
+    </style>
 
 <div class="container mt-5">
     <div class="row justify-content-center">
         <div class="col-md-6">
             <div class="card">
-                <div class="card-header">
-                    <h3>Login</h3>
+                <div class="card-header text-center" >
+                    <h1>Login</h1>
                 </div>
                 <div class="card-body">
                     <form action="proclogin.php" method="POST">
@@ -26,7 +41,7 @@
                             <label for="password">Password</label>
                             <input type="password" class="form-control" id="pass" name="pass" required>
                         </div>
-                        <button type="submit" class="btn btn-primary">Login</button>
+                        <button type="submit" class="btn btn-dark">Login</button>
                     </form>
                 </div>
             </div>
