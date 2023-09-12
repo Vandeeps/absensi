b7f4877524a0f84b2dca27aaeb62d6c2a76f4742
Vandeeps <142085832+Vandeeps@users.noreply.github.com>
2023-09-12 14:56:28 +0800

    tambah bootstrap

--- viewabsen.php
+++ viewabsen.php
@@ -5,11 +5,13 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>DATA KEHADIRAN</title>
-  <link rel="stylesheet" href="style.css" />
+  <link rel="stylesheet" href="style1.css" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
   <link rel="icon" type="image/png" href="icon.jpg">
   <style>
-  body {
-      background-image: url('nc.jpg'); /* Ganti dengan path gambar Anda */
+ body {
+      background-image: url('mantap.png'); /* Ganti dengan path gambar Anda */
       background-size:cover;
       background-position: center;
       background-repeat: repeat;
@@ -17,73 +19,65 @@
     </style>
 </head>
 <body>
-  <!-- NAVIGATION BAR -->
-  <h1 class="judul" align="center">Sistem Data Pegawai</h1>
-  <!-- NAVIGATION BAR -->
-  <div class="container-navbar">
-    <ul class="ul-navbar">
-    <li class="li-navbar">
-        <a href="index.php" class="a-navbar">BERANDA</a>
-      </li>
-      <li class="li-navbar">
-        <a href="viewpegawai.php" class="a-navbar">DATA PEGAWAI</a>
-      </li>
-      <li class="li-navbar">
-      <a href="tambahpegawai.php" class="a-navbar">TAMBAH DATA PEGAWAI</a>
-      </li>
-      <li class="li-navbar">
-        <a href="viewabsen.php" class="a-navbar">ABSENSI</a>
-      </li>
-      <li class="li-navbar">
-        <a href="tambahabsen.php" class="a-navbar">ISI DAFTAR HADIR</a>
-      </li>
-      <li class="li-navbar">
-        <a href="laporan.php" class="a-navbar">DATA LAPORAN KERJA</a>
-      </li>
-      <li class="li-navbar">
-        <a href="tambahlaporan.php" class="a-navbar">LAPORAN KERJA</a>
-      </li>
-    </ul>
+<a class="btn btn-primary" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample">
+  Pindah Halaman
+</a>
+<nav class="navbar navbar-dark bg-dark fixed-top">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Data Daftar Hadir</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+      <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Pilihan Halaman</h5>
+        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body">
+        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="Index.php">Beranda</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="viewpegawai.php">Data Pegawai</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="viewabsen.php">Data Kehadiran</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="laporan.php">Data Laporan</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Input
+            </a>
+            <ul class="dropdown-menu dropdown-menu-dark">
+              <li><a class="dropdown-item" href="tambahpegawai.php">Masukan Data Pegawai</a></li>
+              <li><a class="dropdown-item" href="tambahabsen.php">Isi Daftar Hadir</a></li>
+              <li><a class="dropdown-item" href="tambahlaporan.php">Masukan Laporan Kerja</a></li>
+
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
+</nav>
   <style>
   body {
    
     background-color: #f0f0f0;
   }
 
-  div {
-    margin: 20px auto;
-    width: 80%;
+  table{
+    margin-top:15px;
   }
-
-  table {
-    border-collapse: collapse;
-    width: 80%;
-    border: 1px solid #ccc;
-
-  }
-
-  th, td {
-    padding: 8px;
-    border: 1px solid #ccc;
-  }
-
-  th {
-    background-color: #333 ;
-    color:white;
-  }
-
-  tbody {
-    background-color: gray;
-    text-align: center;
-  }
-  
   
 </style>
-  <h2 class="judulor" align="center">TABEL DATA ABSEN</h2>
       <div>
-        <table border="1" align="right" bgcolor="gray">
-            <thead bgcolor="gray">
+  <table class="table table-dark table-striped-columns"  align="right" bgcolor="gray">
+  <thead bgcolor="gray"
+  align="center">
                   <tr>
                       <th>NO</th>
                       <th>NIP</th>
