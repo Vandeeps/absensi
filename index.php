3575b1c79d0b1e6d120b49bbc594f23a1fa76530
Vandeeps <142085832+Vandeeps@users.noreply.github.com>
2023-09-13 12:39:34 +0800

    jika ada yang kurang bisa di koreksi

--- index.php
+++ index.php
@@ -113,8 +113,61 @@
             </tbody>
         </table>
 </div>
+<Style>
+  footer{
+    margin-top: 500px;
+  }
+</Style>
+<footer>
+		<div class="jumbotron jumbotron-fluid bg-secondary p-4 mt-5 mb-0">
+			<div class="container">
+				<div class="row">
+					<div class="col-12 col-sm-12 col-md-12 col-lg-4 col-xl-4 cizgi">
+						<div class="card bg-secondary border-0">
+							<div class="card-body text-light text-center">
+								<h5 class="card-title text-white display-4" style="font-size:30px">Pembuat Web</h5>
+							<p class="d-inline lead">Vandeeps Corporation © 2023<br>
+							<a href="" class="text-light d-block lead"></a>
+							</p>
+	
+							</div>
+						</div>
+					</div>
+					
+					<div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 cizgi">
+						<div class="card bg-secondary border-0">
+							<div class="card-body text-center">
+								<h5 class="card-title text-white display-4" style="font-size:30px">Kontak : </h5>
+								<a class="text-light d-block lead" style="margin-left: -20px" href="https://wa.me/+62895411192902"><i class="fa fa-phone mr-2"></i>+62-8954-1119-2902</a>
+								<a class="text-light d-block lead" href="#"><i class="fa fa-envelope mr-2"></i>Vandeeps@localhost.com</a>
+							</div>
+						</div>
+					</div>
+					
+					<div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
+						<div class="card bg-secondary border-0">
+							<div class="card-body text-center">
+							<h5 class="card-title text-white display-4" style="font-size:30px">Sosial Media</h5>
+					
+									<a class="text-light" href="#"><i class="fa fa-facebook-square fa-fw fa-2x"></i></a>
+								
+									<a class="text-light" href="https://wa.me/+62895411192902"><i class="fa fa-twitter-square fa-fw fa-2x">Whatsapp</i></a>
+								
+									<a class="text-light" href="https://instagram.com/evannsss._?igshid=OGQ5ZDc2ODk2ZA=="><i class="fa fa-instagram fa-fw fa-2x"></i>Instagram</a>
+								
+									<a class="text-light" href="#"><i class="fa fa-linkedin fa-fw fa-2x"></i></a>
+								
+							</div>
+						</div>
+					</div>	
+				</div>
+			</div>
+		</div>
+	</footer>
+
   
  
 
 </body>
+
 </html>
--- tambahlaporan.php
+++ tambahlaporan.php
@@ -24,7 +24,7 @@
 </a>
 <nav class="navbar navbar-dark bg-dark fixed-top">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Sistem Data Pegawai Serta Daftar Hadir dan Laporan</a>
+    <a class="navbar-brand" href="#">Form Data Laporan Kerja</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -179,7 +179,6 @@ $data = mysqli_fetch_assoc(mysqli_query($koneksi, "select * from data_karyawan w
 ?>
 
   <form action="proclaporan.php" method="post">
-  <h2 class="semijudul" align="center">Data Laporan</h2>
     <table>
     
       <tbody>
@@ -244,5 +243,52 @@ $data = mysqli_fetch_assoc(mysqli_query($koneksi, "select * from data_karyawan w
       location.href = '?NIP=' + ev.value
     }
   </script>
+  <footer>
+		<div class="jumbotron jumbotron-fluid bg-secondary p-4 mt-5 mb-0">
+			<div class="container">
+				<div class="row">
+					<div class="col-12 col-sm-12 col-md-12 col-lg-4 col-xl-4 cizgi">
+						<div class="card bg-secondary border-0">
+							<div class="card-body text-light text-center">
+								<h5 class="card-title text-white display-4" style="font-size:30px">Siapa Yang Membuat?</h5>
+							<p class="d-inline lead">Vandeeps Corporation © 2023<br>
+							<a href="" class="text-light d-block lead"></a>
+							</p>
+	
+							</div>
+						</div>
+					</div>
+					
+					<div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 cizgi">
+						<div class="card bg-secondary border-0">
+							<div class="card-body text-center">
+								<h5 class="card-title text-white display-4" style="font-size:30px">Nomor Pemilik</h5>
+								<a class="text-light d-block lead" style="margin-left: -20px" href="https://wa.me/+62895411192902"><i class="fa fa-phone mr-2"></i>+62-8954-1119-2902</a>
+								<a class="text-light d-block lead" href="#"><i class="fa fa-envelope mr-2"></i>Vandeeps@localhost.com</a>
+							</div>
+						</div>
+					</div>
+					
+					<div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
+						<div class="card bg-secondary border-0">
+							<div class="card-body text-center">
+							<h5 class="card-title text-white display-4" style="font-size:30px">Sosial Media</h5>
+					
+									<a class="text-light" href="#"><i class="fa fa-facebook-square fa-fw fa-2x"></i></a>
+								
+									<a class="text-light" href="https://wa.me/+62895411192902"><i class="fa fa-twitter-square fa-fw fa-2x">Whatsapp</i></a>
+								
+									<a class="text-light" href="https://instagram.com/evannsss._?igshid=OGQ5ZDc2ODk2ZA=="><i class="fa fa-instagram fa-fw fa-2x"></i>Instagram</a>
+								
+									<a class="text-light" href="#"><i class="fa fa-linkedin fa-fw fa-2x"></i></a>
+								
+							</div>
+						</div>
+					</div>	
+				</div>
+			</div>
+		</div>
+	</footer>
+
 </body>
 </html>
